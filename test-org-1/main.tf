7699d2253a9966f8be2033c181ae79aec3e7ef07
Bjarte Helland <bjarte.konglevoll.helland@dnb.no>
2025-04-04 00:58:57 +0200

    output

--- test-org-1/main.tf
+++ test-org-1/main.tf
@@ -2,3 +2,6 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+output "static_message" {
+  value = "Output from test-org-1"
+}
--- test-org-2/main.tf
+++ test-org-2/main.tf
@@ -2,3 +2,7 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+
+output "static_message" {
+  value = "Output from test-org-12
+}
